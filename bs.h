13cb6ccf79ef37e53fa445ec325752746c534282
Thibault Collin <99095146+thibaultcoo@users.noreply.github.com>
2023-12-26 12:44:59 +0100

    Update bs.h
    removed useless comments

--- bs.h
+++ bs.h
@@ -5,20 +5,14 @@ class blackScholes
 {
 public:
     blackScholes(double spot, double strike, double rate, double divs, double vol, double matu);
-
-    // Methods to calculate option prices
     double callOptionPrice();
     double putOptionPrice();
 
 private:
-    // Private methods for internal calculations
     double d1();
     double d2();
-
-    // Standard normal cumulative distribution function
     double normCDF(double value);
 
-    // Member variables
     double spot;
     double strike;
     double rate;
